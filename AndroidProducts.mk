ec6f75cbf8d09363a64809fa48c87a99028b08a2
Raka Prasetya Pratama <123168129+Hirozuto@users.noreply.github.com>
2023-03-26 17:41:29 +0800

    phhgsi: adding AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,15 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/octavi_a64_bvN.mk \
+    $(LOCAL_DIR)/octavi_a64_bgN.mk \
+    $(LOCAL_DIR)/octavi_arm64_bvN.mk \
+    $(LOCAL_DIR)/octavi_arm64_bgN.mk \
+
+COMMON_LUNCH_CHOICES := \
+    octavi_a64_bvN-userdebug \
+    octavi_a64_bvN-eng \
+    octavi_a64_bgN-userdebug \
+    octavi_a64_bgN-eng \
+    octavi_arm64_bvN-userdebug \
+    octavi_arm64_bvN-eng \
+    octavi_arm64_bgN-userdebug \
+    octavi_arm64_bgN-eng
